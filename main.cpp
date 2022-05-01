12ab7c3b21b8dd5267bd9a099d6caac90ad5f9f5
Sergey119 <78952666+Sergey119@users.noreply.github.com>
2022-05-01 15:39:23 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>	// для оператора cout
+#include <cctype>   // для функции isdigit
+#include <stdlib.h> // для функции atoi
+#include "windows.h"// для функции Sleep()
+#include <vector>	// для структуры node
+
+//Выравнивание потока
+void stream_equalization(int originalNumber, char * str)
+{
+	int atoi(*str);
+	int amountOf0 = 0;
+	int copyOriginalNumber = originalNumber;
+	//Перевод из десятичной
+	//с.с. в двоичную, но
+	//задом наперед
+	int changeling = 0;
+	while (originalNumber)
+	{
+		changeling = changeling * 10 +
+			(originalNumber % 2);
+		//std::cout << "originalNumber: " << originalNumber << std::endl;
+		/*if (originalNumber % 2 == 0)
+			amountOf0++;
+		if (((originalNumber / 2) % 2) == 1)
+			amountOf0 = 0;
+		std::cout << "amountOf0: " << amountOf0 << std::endl;*/
+		originalNumber /= 2;
+	}
+	originalNumber = copyOriginalNumber;
+	if (originalNumber % 2 == 0)
+		while (originalNumber % 2 == 0)
+		{
+			amountOf0++;
+			originalNumber /= 2;
+		}
+	std::cout << "amountOf0: " << amountOf0 << std::endl;
+	//Переворачиваем 
+	//двоичное число,
+	//записанное наоборот
+	int binaryNumber = 0;
+	while (changeling)
+	{
+		binaryNumber = binaryNumber * 10 +
+			(changeling % 10);
+		changeling /= 10;
+	}
+	//Если были в конце
+	//числа нули, их
+	//нужно дописать
+	std::cout << "binaryNumber до: " << binaryNumber << std::endl;
+	if (amountOf0 != 0)
+		for (int i = 0; i < amountOf0; i++)
+		{
+			binaryNumber = binaryNumber*10;
+		}
+
+	std::cout << "binaryNumber после: " << binaryNumber << std::endl;
+
+	binaryNumber = binaryNumber * 10 + 1;
+	std::cout << "binaryNumber & add one bit: " << binaryNumber << std::endl;
+	originalNumber = copyOriginalNumber;
+	int moduloNumber = originalNumber;
+	/*while (originalNumber % 512 != 448)
+	{
+		binaryNumber = binaryNumber * 10;
+		std::cout << "binaryNumber: " << binaryNumber;
+	}*/
+	while (moduloNumber % 512 != 448)
+		moduloNumber++;
+		
+	std::cout << "difference between original and modulo number: "
+		<< (moduloNumber-originalNumber) << std::endl;
+	
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int acceptedNumber = 0;
+	std::cin >> acceptedNumber;
+	std::cout << std::endl;
+	stream_equalization(acceptedNumber);
+}
